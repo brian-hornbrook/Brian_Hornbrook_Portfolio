780541bc0fe2817e58a12f9f78b7bbe18b67c4cb
Brian Hornbrook <brian_hornbrook@outlook.com>
2026-04-25 17:49:56 -0500

    structured devops page

--- portfolio/app/urls.py
+++ portfolio/app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('repos', views.repos, name='repos'),
+    path('devops', views.devops, name='devops'),
     path('canva', views.canva, name='canva'),
     path('claude', views.claude, name='claude'),
 ]
--- portfolio/app/views.py
+++ portfolio/app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
+def devops(request):
+    return render(request, 'devops/devops.html')
+
 def repos(request):
     return render(request, 'repos.html')
 
